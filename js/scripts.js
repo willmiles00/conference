fdea3fe522c2e75d30d9f2e39a263e8b66fd4d35
Will <113927655+willmiles00@users.noreply.github.com>
2023-12-06 00:00:36 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const hb = document.querySelector('#hamburgerBtn');
+const pn = document.querySelector('#primaryNav');
+
+hb.addEventListener('click', () => {
+    hb.classList.toggle('open');
+    pn.classList.toggle('open');
+}
+);
